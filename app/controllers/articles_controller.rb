49685658b65d44dcd502d5edf5d548197e4e7219
sumit <iamsumitgoyal1@gmail.com>
2020-08-18 16:17:36 +0530

    Performed crud on articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,14 +8,40 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    # @article = Article.new()  
+    @article = Article.new
   end
 
-  def create; end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
 
-  def delete; end
+  def edit
+    @article = Article.find(params[:id])
+   end
 
-  def update; end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
 
-  def edit; end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
